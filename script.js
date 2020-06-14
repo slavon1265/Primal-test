ec2a8d8b6508ca936ef260de9c9af77aa44f2cd8
slavon1265 <slavon1265@gmail.com>
2020-06-14 10:41:35 +0300

    renewed

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-window.onload = () => {
-    initState()
+// window.onload = () => {
+//     initState()
+// }
+// function initState() {
+//     initialState = {
+//         users: {
+//             count:0
+//         },
+//     };
+//     if (!localStorage.hasOwnProperty('state')){
+//         localStorage.state = JSON.stringify(initialState);
+//     }
+// }
+// function storeGet(){
+//
+//     const state = JSON.parse(localStorage.state);
+//     return state
+//
+// }
+// function storeSet(target, ){
+//     const state = JSON.parse(localStorage.state);
+// }
+
+let sizeCntr = document.querySelector('#size-ctr');
+
+sizeCntr.addEventListener('input', (e)=>{
+    console.log(e)
+})
+
+let delay = ms => {
+    return new Promise(resolve => ()=> setTimeout(()=>{console.log('delayed:', ms)},ms))
 }
-function initState() {
-    initialState = {
-        users: {
-            count:0
-        },
-    };
-    if (!localStorage.hasOwnProperty('state')){
-        localStorage.state = JSON.stringify(initialState);
-    }
-}
-function storeGet(){
-
-    const state = JSON.parse(localStorage.state);
-    return state
-
-}
-function storeSet(target, ){
-    const state = JSON.parse(localStorage.state);
+delay(4000).then(console.log(12))
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
